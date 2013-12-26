ffbcf0fdb3c48f57aed7fdcb5ed0ee4c0b7e9af0
Matt Dressel <matt.dressel@gmail.com>
2013-12-26 17:04:50 -0600

    Completed documentation of Utilities

--- lib/memtf/utilities.rb
+++ lib/memtf/utilities.rb
@@ -1,3 +1,4 @@
+# Common utilities
 module Memtf::Utilities
 end
 
--- lib/memtf/utilities/array.rb
+++ lib/memtf/utilities/array.rb
@@ -1,10 +1,15 @@
+# Monkeypatch Array with various utilities.
 module Memtf::Utilities::Array
+
+  # Add up all array elements.
+  #
   # @return [Number]
   def sum
 		inject(0) { |sum, elem| sum + elem }
 	end
 end
 
+# Only monkeypatch if the method has not been defined already.
 unless Array.method_defined? :sum
 	Array.send :include, Memtf::Utilities::Array
 end
