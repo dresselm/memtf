eb4a1e0b339197a6e2fde8824fa9297faaf57295
Matt Dressel <matt.dressel@gmail.com>
2013-12-26 22:21:31 -0600

    Added pending Analyzer specs

--- spec/lib/memtf/analyzer_spec.rb
+++ spec/lib/memtf/analyzer_spec.rb
@@ -2,14 +2,22 @@ require 'spec_helper'
 
 describe Memtf::Analyzer do
   describe '.analyze' do
-    it 'should analyze'
+    it 'should create a new Analyzer'
+    it 'should delegate to analyze'
   end
 
   describe '.analyze_group' do
-    it 'should analyze the group'
+    it 'should load the start data'
+    it 'should load the end data'
+    it 'should compare the start and end data'
+    it 'should generate an impact value'
   end
 
   describe '#analyze' do
-    it 'should analyze'
+    it 'should try to garbage collect'
+    it 'should calculate the total memory allocated to each class'
+    it 'should isolate classes that exceed a given memory threshold'
+    it 'should generate an aggregated Others* row'
+    it 'should translate the results into a simple hash'
   end
 end
