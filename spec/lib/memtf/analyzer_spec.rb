d813661add1156f3171c01c5bffae8ce1fd6b668
Matt Dressel <matt.dressel@gmail.com>
2013-12-26 22:14:44 -0600

    Added analyzer spec placeholoders

---
+++ spec/lib/memtf/analyzer_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Memtf::Analyzer do
+  describe '.analyze' do
+    it 'should analyze'
+  end
+
+  describe '.analyze_group' do
+    it 'should analyze the group'
+  end
+
+  describe '#analyze' do
+    it 'should analyze'
+  end
+end
